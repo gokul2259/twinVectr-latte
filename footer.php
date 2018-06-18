d336a8758a82a8dce1bb030bbdb9fef59a6bbc02
Gokul2259 <gokul2259@gmail.com>
2018-06-17 23:11:39 -0400

    gokul-webdeveloper initial

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+		<footer class="footer" id="footer">
+			<div class="row">
+				<div class="col-md-12">
+					<?php echo '<p> © 2016 GokulaKrishnan. All rights reserved. WordPress</p>'; ?>
+				</div>
+			</div>
+		</footer>
+
+	</div>
+<?php wp_footer(); ?>
+ </body>
+</html>
