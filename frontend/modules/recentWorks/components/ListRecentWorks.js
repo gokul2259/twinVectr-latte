14e83f813b75766453a958369073be156c6147fc
Gokul2259 <gokul2259@gmail.com>
2018-07-14 23:56:47 -0400

    added ESLint config and vscode debug

--- frontend/modules/recentWorks/components/ListRecentWorks.js
+++ frontend/modules/recentWorks/components/ListRecentWorks.js
@@ -1,26 +1,26 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 class ListRecentWorks extends Component {
 
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    const { title, subTitle } = this.props;
-    return(
-      <div>
-        <div className="container">
-				    <div className="row">
-              <header data-sr="ease-in-out wait 0.25s" className="services-header">
-                <h2>{title}</h2>
-                  <h3>{subTitle}</h3>
-              </header>
+    render() {
+        const {title, subTitle} = this.props;
+        return (
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <header data-sr="ease-in-out wait 0.25s" className="services-header">
+                            <h2>{title}</h2>
+                            <h3>{subTitle}</h3>
+                        </header>
+                    </div>
+                </div>
             </div>
-          </div>
-      </div>
-    );
-  }
+        );
+    }
 
 }
 
