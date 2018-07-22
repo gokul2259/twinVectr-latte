418272b3139d7d896bcdb8741d32202fcb822570
Ahilan <ahilan@Amalans-MacBook-Pro.local>
2018-07-22 13:22:58 -0400

    set dynamic intro and footer

--- sections/intro.php
+++ sections/intro.php
@@ -11,8 +11,8 @@
 						<div class="avatar" style="background-image:url('<?php echo esc_url($latte_intro_avatar); ?>');"></div>
 					<?php endif; ?>
 						<!-- <h1 class="cover-heading"><?php bloginfo( 'name' ); ?></h1> -->
-                                                 <h1  class="cover-heading">GOKULAKRISHNAN</h1>
-                                                  <h2 class="">Wordpress/PHP developer</h2>
+                                                 <h1 class="cover-heading"><?= bloginfo( 'name' )?></h1>
+                                                  <h2 class=""><?= bloginfo( 'description' )?></h2>
 						<p class="lead"><?php bloginfo( 'description' ); ?></p>
 						<?php if(!empty($latte_intro_scroll)) : ?>
 							<a href="<?php echo esc_url($latte_intro_scroll); ?>" class="arrow"><i class="fa fa-arrow-circle-down"></i></a>
