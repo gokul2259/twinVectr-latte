1a8e24016d0c6a9f559fb2bae3fe6991f1939051
Ahilan Gopalakrishnan <ahilangopal@gmail.com>
2018-08-04 19:44:24 -0400

    Added Parallax to the Hero title

--- frontend/components/video/index.js
+++ frontend/components/video/index.js
@@ -1,44 +1,58 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class Video extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+    this.handleScrollCalcualtion = this.handleScrollCalcualtion.bind(this);
+  }
 
-    render() {
-        const {
-            videoMp4, 
-            height, 
-            width,
-            autoPlay,
-            controls,
-            loop,
-            muted,
-        } = this.props;
- 
-        return (
-            <video
-            height={height} 
-            width={width} 
-            autoPlay={autoPlay} 
-            loop={loop} 
-            muted={muted}
-            controls={controls}
-            > 
-                <source src={videoMp4} type='video/mp4'/>
-                <source src='http://localhost/wp-content/uploads/2018/07/sample.webm' type='video/webm'/>
-            </video>
-        );
-    }
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScrollCalcualtion);
+  }
+
+  handleScrollCalcualtion(event) {
+    const vScrollTop = event.target.scrollingElement.scrollTop;
+    const heroText = document.querySelector(".cover-heading");
+    heroText.style.transform = this.getScrollPosition(vScrollTop);
+  }
+
+  getScrollPosition(scrollTop) {
+    return `translate(0px, ${scrollTop / 2}%)`;
+  }
+
+  render() {
+    const {
+      videoMp4,
+      height,
+      width,
+      autoPlay,
+      controls,
+      loop,
+      muted
+    } = this.props;
+
+    return (
+      <video
+        height={height}
+        width={width}
+        autoPlay={autoPlay}
+        loop={loop}
+        muted={muted}
+        controls={controls}
+      >
+        <source src={videoMp4} type="video/mp4" />
+      </video>
+    );
+  }
 }
 
 Video.defaultProps = {
-    height: '500',
-    width: '1200',
-    autoPlay: false,
-    controls: false,
-    loop: false,
-    muted: false,
-  };
+  height: "500",
+  width: "1200",
+  autoPlay: false,
+  controls: false,
+  loop: false,
+  muted: false
+};
 
 module.exports = Video;
--- sections/intro.php
+++ sections/intro.php
@@ -10,7 +10,7 @@ get_template_directory_uri().'/assets/images/avatar.jpg' ); $latte_intro_scroll
                 <h2 class=""><?= bloginfo( 'description' )?></h2>
                 <?php if(!empty($latte_intro_scroll)) : ?>
                 <a href="<?php echo esc_url($latte_intro_scroll); ?>" class="arrow">
-                    <i class="fa fa-arrow-circle-down"></i>
+                    <i class="fa fa-angle-down"></i>
                 </a>
                 <?php endif; ?>
             </div>
