2b24b7b4487075a8d80eb32c9a016b50107e9ff2
twinVectr <gokul2259@gmail.com>
2018-09-15 12:44:44 -0400

    circular bar changes

--- sections/about.php
+++ sections/about.php
@@ -9,9 +9,5 @@
 	$latte_about_content = esc_html(get_theme_mod('latte_about_content',__( '<p>Latte is a one-page parallax WordPress theme for developers, designers & freelancers, to showcase their profile and portfolio.</p> <p>It comes with many options, including services section, portfolio, maps, contact form, testimonials, pricing tables, and more.</p> <p>Plus, it\'s easy to customize! You don\'t need a page builder or a drag & drop editor. Every thing can be customized straight from the WordPress Customizer.</p>', 'latte' )));
 	?>
 
-
-	<?php echo header('Content-Type: application/json; charset=utf-8'); ?>
-
-<div id="about" class="parllax-image-text" js-react-module="parllaxImageText" data-react-props='<?php echo json_encode(['title' => $latte_about_title, 'subtitle'=> $latte_about_subtitle, 'mainImage'=> $latte_about_avatar, 'subImageOne' => $latte_about_image1, 'subImageTwo' => $latte_about_image2, 'aboutName' => $latte_about_name, 'aboutContent' => $latte_about_content]) ?>' > 
-</div>
+	<div id="about" class="parllax-image-text" js-react-module="parllaxImageText" data-react-props='<?php echo json_encode(['title' => $latte_about_title, 'subtitle'=> $latte_about_subtitle, 'mainImage'=> $latte_about_avatar, 'subImageOne' => $latte_about_image1, 'subImageTwo' => $latte_about_image2, 'aboutName' => $latte_about_name, 'aboutContent' => $latte_about_content]) ?>' >  </div>
 
